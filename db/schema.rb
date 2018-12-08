ebdb4808e8fabf8f3a765bb8cee51d92643650ab
Neha Verma <neha.verma@yale.edu>
2018-12-08 14:00:21 -0500

    added survey model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609220802) do
+ActiveRecord::Schema.define(version: 20181208184649) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 20160609220802) do
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "surveys", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
